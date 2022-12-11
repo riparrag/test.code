98c2fd08668e3d5560a9bc4d526977e18ae43642
riparrag <rodrigo.ipa82@gmail.com>
2022-12-11 17:36:51 -0300

    riparrag - matrix

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -12,9 +12,20 @@ export const Test = () => {
 
     function test() {
         //-----------------------------------------------------------------------------
-        let array = [5,3,4,2,1];
-        console.log('test2', test2(array) );
+        let array = Array(4);
+        array[0] = [112, 42, 83, 119];
+        array[1] = [56, 125, 56, 49];
+        array[2] = [15, 78, 101, 43];
+        array[3] = [62, 98, 114, 108];
+        console.log('test2', flippingMatrix(array) );
         
+        array = Array(4);
+        array[0] = [107, 54, 128, 15];
+        array[1] = [12, 75, 110, 138];
+        array[2] = [100, 96, 34, 85];
+        array[3] = [75, 15, 28, 112];
+        console.log('test2', flippingMatrix(array) );
+
         //-----------------------------------------------------------------------------
 /*        array = [5,3,4,2,1];
         console.log('findMedian', findMedian(array) );
@@ -46,7 +57,26 @@ export const Test = () => {
 */
     }
 
-    function test2(arr) {
+    function flippingMatrix(matrix) {
+        //reverse col 2
+        for (let i=0; i < matrix.length/2; i++) {
+            let aux = matrix[i][2];
+            matrix[i][2] = matrix[matrix.length-1-i][2];
+            matrix[matrix.length-1-i][2] = aux; 
+        }
+        //reverse row 0
+        for (let i=0; i < matrix.length/2; i++) {
+            let aux = matrix[0][i];
+            matrix[0][i] = matrix[0][matrix.length-1-i];
+            matrix[0][matrix.length-1-i] = aux;
+        }
+        let sum = 0;
+        for (let i=0; i<matrix.length/2; i++) {
+            for (let j=0; j<matrix.length/2; j++) {
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
     }
     
     function findMedian(arr) {
@@ -56,6 +86,28 @@ export const Test = () => {
         return arr[ Math.floor(arr.length/2) ];
     }
 
+    function fizzBuzz(n) {
+        for (let i=1; i<=n; i++) {
+            let isMultipleOf3 = i % 3 == 0;
+            let isMultipleOf5 = i % 5 == 0;
+            if (isMultipleOf3) {
+                if (isMultipleOf5) {
+                    console.log('FizzBuzz');
+                }
+                else {
+                    console.log('Fizz');
+                }
+            }
+            else {
+                if (isMultipleOf5) {
+                    console.log('Buzz');
+                }
+            }
+            if (!isMultipleOf3 && !isMultipleOf5) {
+                console.log(i);
+            }
+        }
+    }
 
     function countingSort(array) {
         let countingArray = Array(100).fill(0);
